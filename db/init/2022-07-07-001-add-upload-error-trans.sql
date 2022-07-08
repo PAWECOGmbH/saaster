d903c3bb36bc24974d3910f2f769b927acb805a3
rbuechler <97615157+rbuechler@users.noreply.github.com>
2022-07-08 11:39:00 +0200

    Changed english translations

--- db/init/2022-07-07-001-add-upload-error-trans.sql
+++ db/init/2022-07-07-001-add-upload-error-trans.sql
@@ -2,6 +2,6 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 INSERT INTO system_translations (strVariable, strStringDE, strStringEN)
-VALUES ('msgFileUploadError', 'Fehler beim hochladen der Datei!', 'Error uploading the file!');
+VALUES ('msgFileUploadError', 'Fehler beim hochladen der Datei!', 'Error while uploading the file!');
 
 SET FOREIGN_KEY_CHECKS = 1;
