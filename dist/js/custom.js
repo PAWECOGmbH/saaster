d3e8eb480db306501b97c4541abeb00809922670
rbuechler <97615157+rbuechler@users.noreply.github.com>
2022-12-21 15:23:13 +0100

    Added API key generator

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -326,3 +326,27 @@ $(document).ready(function(){
 
 
 });
+
+function generateUUID()
+{
+	var d = new Date().getTime();
+	
+	if( window.performance && typeof window.performance.now === "function" )
+	{
+		d += performance.now();
+	}
+	
+	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c)
+	{
+		var r = (d + Math.random()*16)%16 | 0;
+		d = Math.floor(d/16);
+		return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+	});
+
+return uuid;
+}
+
+$( '#keygen' ).on('click',function()
+{
+	$( '#apiKey' ).val( generateUUID() );
+});
