b9123e7907e2dba99c2619a60142438696bad40b
dwilli241 <dw@paweco.ch>
2023-04-28 15:26:14 +0200

    28.04.2023/2

--- db/init/2023-04-21-ticketsystem.sql
+++ db/init/2023-04-21-ticketsystem.sql
@@ -2,47 +2,47 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 /* Create tabelle status */
-DROP TABLE IF EXISTS 'ticket_status';
-CREATE TABLE 'ticket_status'  (
-  'intStatusID' int(11) NOT NULL AUTO_INCREMENT,
-  'strName' varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  PRIMARY KEY ('intStatusID') USING BTREE
+DROP TABLE IF EXISTS `ticket_status`;
+CREATE TABLE `ticket_status`  (
+  `intStatusID` int(11) NOT NULL AUTO_INCREMENT,
+  `strName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`intStatusID`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 /* Create tabelle ticket */
-DROP TABLE IF EXISTS 'ticket';
-CREATE TABLE 'ticket'  (
-  'intTicketID' int(11) NOT NULL AUTO_INCREMENT,
-  'intStatusID' int(11) NOT NULL,
-  'intUserID' int(11) NOT NULL,
-  'intWorkerID' int(11) NULL DEFAULT NULL,
-  'strReference' varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  'strDescription' varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  'dtmOpen' datetime NULL DEFAULT NULL,
-  'strUUID' varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY ('intTicketID') USING BTREE,
-  INDEX '_intStatusID'('intStatusID') USING BTREE,
-  INDEX '_intUserID'('intUserID') USING BTREE,
-  INDEX '_intWorkerID'('intWorkerID') USING BTREE,
-  INDEX '_strUUID'('strUUID') USING BTREE,
-  CONSTRAINT 'frn_tick_ticket_status' FOREIGN KEY ('intStatusID') REFERENCES 'ticket_status' ('intStatusID') ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT 'frn_tick_users_1' FOREIGN KEY ('intUserID') REFERENCES 'users' ('intUserID') ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT 'frn_tick_users_2' FOREIGN KEY ('intWorkerID') REFERENCES 'users' ('intUserID') ON DELETE SET NULL ON UPDATE NO ACTION
+DROP TABLE IF EXISTS `ticket`;
+CREATE TABLE `ticket`  (
+  `intTicketID` int(11) NOT NULL AUTO_INCREMENT,
+  `intStatusID` int(11) NOT NULL,
+  `intUserID` int(11) NOT NULL,
+  `intWorkerID` int(11) NULL DEFAULT NULL,
+  `strReference` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `strDescription` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `dtmOpen` datetime NULL DEFAULT NULL,
+  `strUUID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`intTicketID`) USING BTREE,
+  INDEX `_intStatusID`(`intStatusID`) USING BTREE,
+  INDEX `_intUserID`(`intUserID`) USING BTREE,
+  INDEX `_intWorkerID`(`intWorkerID`) USING BTREE,
+  INDEX `_strUUID`(`strUUID`) USING BTREE,
+  CONSTRAINT `frn_tick_ticket_status` FOREIGN KEY (`intStatusID`) REFERENCES `ticket_status` (`intStatusID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `frn_tick_users_1` FOREIGN KEY (`intUserID`) REFERENCES `users` (`intUserID`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `frn_tick_users_2` FOREIGN KEY (`intWorkerID`) REFERENCES `users` (`intUserID`) ON DELETE SET NULL ON UPDATE NO ACTION
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 /* Create tabelle answer */
-DROP TABLE IF EXISTS 'ticket_answer';
-CREATE TABLE 'ticket_answer'  (
-  'intAnswerID' int(11) NOT NULL AUTO_INCREMENT,
-  'intTicketID' int(11) NOT NULL,
-  'intUserID' int(11) NOT NULL,
-  'strAnswer' varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  'dtmSent' datetime NULL DEFAULT NULL,
-  PRIMARY KEY ('intAnswerID') USING BTREE,
-  INDEX '_intTicketID'('intTicketID') USING BTREE,
-  INDEX '_intUserID'('intUserID') USING BTREE,
-  CONSTRAINT 'frn_ta_ticket' FOREIGN KEY ('intTicketID') REFERENCES 'ticket' ('intTicketID') ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT 'frn_ta_users' FOREIGN KEY ('intUserID') REFERENCES 'users' ('intUserID') ON DELETE CASCADE ON UPDATE NO ACTION
+DROP TABLE IF EXISTS `ticket_answer`;
+CREATE TABLE `ticket_answer`  (
+  `intAnswerID` int(11) NOT NULL AUTO_INCREMENT,
+  `intTicketID` int(11) NOT NULL,
+  `intUserID` int(11) NOT NULL,
+  `strAnswer` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `dtmSent` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`intAnswerID`) USING BTREE,
+  INDEX `_intTicketID`(`intTicketID`) USING BTREE,
+  INDEX `_intUserID`(`intUserID`) USING BTREE,
+  CONSTRAINT `frn_ta_ticket` FOREIGN KEY (`intTicketID`) REFERENCES `ticket` (`intTicketID`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `frn_ta_users` FOREIGN KEY (`intUserID`) REFERENCES `users` (`intUserID`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 /* System mapping entries */
