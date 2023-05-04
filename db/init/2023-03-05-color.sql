2522d66e01508a721b7d906664435404772e2496
dwilli241 <dw@paweco.ch>
2023-05-04 11:37:31 +0200

    Initialized basic table color

--- db/init/2023-03-05-color.sql
+++ db/init/2023-03-05-color.sql
@@ -2,7 +2,7 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 INSERT INTO `system_settings` (strSettingVariable, strDefaultValue, strDescription)
-VALUES ('settingColorPrimary', '#000000', 'Choose a primary color:'),
-('settingColorSecondary', '#000000', 'Choose a secondary color:');
+VALUES ('settingColorPrimary', '#206bc4', 'Choose a primary color:'),
+('settingColorSecondary', '#626976', 'Choose a secondary color:');
 
 SET FOREIGN_KEY_CHECKS = 1;
