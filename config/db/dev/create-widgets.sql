dfedfe8d493895da71bee62a618cb83c1c0bd6fe
ptruessel <pt@paweco.ch>
2024-10-08 15:33:36 +0200

    SQL adjustments due to MySQL 8

--- config/db/dev/create-widgets.sql
+++ config/db/dev/create-widgets.sql
@@ -6,12 +6,12 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `widgets`;
 CREATE TABLE `widgets`  (
-  `intWidgetID` int(11) NOT NULL AUTO_INCREMENT,
+  `intWidgetID` int NOT NULL AUTO_INCREMENT,
   `strWidgetName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `strFilePath` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `blnActive` tinyint(1) NOT NULL DEFAULT 1,
-  `intRatioID` int(2) NULL DEFAULT 1,
-  `blnPermDisplay` tinyint(1) NULL DEFAULT 1,
+  `blnActive` tinyint NOT NULL DEFAULT 1,
+  `intRatioID` int NULL DEFAULT 1,
+  `blnPermDisplay` tinyint NULL DEFAULT 1,
   PRIMARY KEY (`intWidgetID`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
