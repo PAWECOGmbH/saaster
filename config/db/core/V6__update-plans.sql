5aff2868831a107019f3f3828fa2921fa33e305e
phBucher0 <pb@paweco.ch>
2024-12-23 15:52:39 +0100

    enhancment: add redirect to plans, fix: redirect on modules with prices

---
+++ config/db/core/V6__update-plans.sql
@@ -0,0 +1,3 @@
+-- Hinzufügen des Feldes strRedirect zur Tabelle plans
+ALTER TABLE `plans`
+ADD COLUMN `strRedirectPath` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL AFTER `strBookingLink`;
